1e4b8a6393b5c34b192e82293e87757b3415e1e4
Manato Takahashi <154682191+manato-takahashi@users.noreply.github.com>
2025-03-04 06:19:35 +0000

    add: edit, updateアクションの保護まで完了

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,6 +29,11 @@ module SessionsHelper
     end
   end
 
+  # 渡されたユーザーがcurrent_userであればtrueを返す
+  def current_user?(user)
+    user && user == current_user
+  end
+
   # ユーザーがログインしていればtrue, その他ならfalseを返す
   def logged_in?
     !current_user.nil?
