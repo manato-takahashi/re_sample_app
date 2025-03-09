63700a4f11aa8ab39b129ba98e06ae6ace412ce4
Manato Takahashi <154682191+manato-takahashi@users.noreply.github.com>
2025-03-09 03:13:39 +0000

    add: Railsのメールプレビュー機能を使ってプレビューを確認できるように

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  host = 'stunning-umbrella-4jjr9j6j6q9qfvg9-3000.app.github.dev' # CodespaceのクラウドIDE
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
